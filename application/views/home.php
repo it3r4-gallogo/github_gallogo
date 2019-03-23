0b275e10a6b28755987e57f516609b87224686b5
it3r4-gallogo <richegals4@gmail.com>
2019-03-23 16:41:39 +0800

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> Lab activity Git and GitHUB </title>
+<link href="assets/css/mystyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1> This is  my github activity lab - Gallogo</h1>
+	<script src="<?php echo "assets/js/myscript.js"; ?>" ></script>
+</body>
+</html>
